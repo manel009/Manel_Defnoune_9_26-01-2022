e2006123a8928db757005730d70fb2f011bc5493
Manel.Defnoune <36042435+manel009@users.noreply.github.com>
2022-02-02 00:29:03 +0100

    Correction bug report odre bills

--- FRONT/src/views/BillsUI.js
+++ FRONT/src/views/BillsUI.js
@@ -5,7 +5,7 @@ import LoadingPage from "./LoadingPage.js"
 import Actions from './Actions.js'
 
 const row = (bill) => {
-  return (`
+    return (`
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
@@ -17,15 +17,15 @@ const row = (bill) => {
       </td>
     </tr>
     `)
-  }
+}
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+    return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
-  
-  const modal = () => (`
+
+    const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -42,13 +42,15 @@ export default ({ data: bills, loading, error }) => {
     </div>
   `)
 
-  if (loading) {
-    return LoadingPage()
-  } else if (error) {
-    return ErrorPage(error)
-  }
-  
-  return (`
+    if (loading) {
+        return LoadingPage()
+    } else if (error) {
+        return ErrorPage(error)
+    }
+
+    bills.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    return (`
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -75,6 +77,5 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
       ${modal()}
-    </div>`
-  )
+    </div>`)
 }
