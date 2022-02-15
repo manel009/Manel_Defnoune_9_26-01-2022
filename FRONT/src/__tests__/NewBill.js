4793a5b77f7ae6a1059a6435c8ab65107af48ec2
Manel.Defnoune <36042435+manel009@users.noreply.github.com>
2022-02-15 11:49:08 +0100

    Test NewBill avec formulaire valide

--- FRONT/src/__tests__/NewBill.js
+++ FRONT/src/__tests__/NewBill.js
@@ -3,7 +3,7 @@
  */
 
 import '@testing-library/jest-dom'
-import { screen } from "@testing-library/dom"
+import { screen, fireEvent } from "@testing-library/dom"
 import userEvent from '@testing-library/user-event'
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
@@ -103,5 +103,59 @@ describe("Given I am connected as an employee", () => {
                 expect(window.alert).toBeCalledWith("Format de fichier invalide !");
             })
         })
+
+        describe("When I complete the form ", () => {
+            test("If I submit a correct form, it should create a new bill and return to the Bills page", () => {
+                // initialisation
+                const onNavigate = (pathname) => { document.body.innerHTML = ROUTES({ pathname }) }
+                const html = NewBillUI()
+                document.body.innerHTML = html
+
+                let containerNewBill = new NewBill({
+                    document,
+                    onNavigate,
+                    store: null,
+                    localStorage: window.localStorage,
+                })
+
+                // création d'un bill de test
+                const email = JSON.parse(localStorage.getItem('user')).email
+                const testBill = {
+                    email,
+                    type: 'Transports',
+                    name: 'Test',
+                    amount: 100,
+                    date: '2022-01-28',
+                    vat: '70',
+                    pct: 20,
+                    commentary: 'Test commentaire',
+                    fileUrl: 'https://test.com/test.jpg',
+                    fileName: 'test.jpg',
+                    status: 'pending'
+                }
+
+                // Remplissage du formulaire avec les valeurs du test
+                const form = screen.getByTestId("form-new-bill")
+                screen.getByTestId("expense-type").value = testBill.type
+                screen.getByTestId("expense-name").value = testBill.name
+                screen.getByTestId("datepicker").value = testBill.date
+                screen.getByTestId("amount").value = testBill.amount
+                screen.getByTestId("vat").value = testBill.vat
+                screen.getByTestId("commentary").value = testBill.commentary
+                containerNewBill.fileUrl = 'https://test.com/test.jpg'
+                containerNewBill.fileName = 'test.jpg'
+
+                // mock de la methode handleSubmit 
+                const handleSubmit = jest.spyOn(containerNewBill, 'handleSubmit')
+                containerNewBill.createBill = (containerNewBill) => containerNewBill
+                form.addEventListener('submit', handleSubmit)
+                fireEvent.submit(form)
+
+                // on verifie que le submit a bien ete appele et qu'on est bien revenu sur "Mes notes de frais"
+                expect(handleSubmit).toHaveBeenCalled()
+                expect(screen.getByText(/Mes notes de frais/i)).toBeTruthy()
+            })
+        })
+
     })
 })
